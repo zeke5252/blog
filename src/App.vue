228e5ed5096c7067b6b2e1d11faa7bcfdb83339d
Zeke Lin <zeke5252@yahoo.com.tw>
2021-10-14 10:26:34 +0800

    Fixed abnormal logo animation

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,10 @@
     <main class="col-12 col-md-11 offset-md-1">
       <router-view/>
     </main>
-    <div v-if="isMobileMenu" class="w-100 h-100 fixed-top" style= "background-color: black; opacity: .3;" @click="doMenu"></div>
+    <div v-if="isMobileMenu" class="w-100 h-100 fixed-top" style= "background-color: black; opacity: .3;" @click="doMenu" ></div>
     <nav :class="!isMobileMenu ? 'd-none d-md-block col-1 p-0 fixed-top justify-content-center' : 'col-3 px-4 fixed-top h-100 mobileBg'" >
         <router-link to="/">
-          <svg class=" my-4 px-sm-3 px-md-3 px-lg-4" style="width: 100%; height: 50px; margin: auto">
-            <use href="#logo"></use>
-          </svg>
+          <Logo class="my-4 px-sm-3 px-md-3 px-lg-4" style="width: 100%; height: 50px; margin: auto" clipId="desktop"/>
         </router-link>
         <ul class="p-0">
           <router-link to="/" class="mx-auto">
@@ -23,49 +21,13 @@
           </router-link>
         </ul>
     </nav>
-    <svg class= "logo--m d-block d-md-none p-0 fixed-top mt-1 mx-2" style="width: 40px; height: 40px;" @click="doMenu">
-      <use href="#logo"></use>
-    </svg>
+    <Logo class="logo--m d-block d-md-none p-0 fixed-top mt-1 mx-2" style="width: 40px; height: 40px;" :doMenu="doMenu" clipId="mobile" />
   </div>
 </div>
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-	<symbol id="logo" x="0" y="0" viewBox="0 0 342 131.9" >
-    <defs>
-      <circle id="clipJP" cx="64.3" cy="67.5" r="64.3"/>
-    </defs>
-    <clipPath id="clipJP_1_">
-      <use xlink:href="#clipJP"  style="overflow:visible;"/>
-    </clipPath>
-    <g id="jpGroup">
-      <path id="P" class="st" d="M242.1,0h44.9c30.3,0,53.6,10.4,53.6,42.8c0,31.3-23.3,45.2-52.9,45.2h-18.4v40.7h-27.1V0z M285.9,66.4 c18.8,0,27.8-8,27.8-23.7c0-16-10.1-21.2-28.9-21.2h-15.3v44.9H285.9z"/>
-      <path id="J" class="st" d="M175.6,0v82.4c0,18.8-6.3,25.4-18.1,25.4c-1.7,0-3.5-0.3-5.2-0.7c-3.1,7.3-7.7,14.3-12.5,20.2 c6.3,2.4,12.9,4.2,21.2,4.2c29.2,0,42.1-20.9,42.1-46.6V0H175.6z"/>
-    </g>
-    <path id="bgP" class="st0" d="M242.1,0h44.9c30.3,0,53.6,10.4,53.6,42.8c0,31.3-23.3,45.2-52.9,45.2h-18.4v40.7h-27.1V0z M285.9,66.4 c18.8,0,27.8-8,27.8-23.7c0-16-10.1-21.2-28.9-21.2h-15.3v44.9H285.9z"/>
-    <path id="bgJ" class="st0" d="M175.6,0v82.4c0,18.8-6.3,25.4-18.1,25.4c-1.7,0-3.5-0.3-5.2-0.7c-3.1,7.3-7.7,14.3-12.5,20.2 c6.3,2.4,12.9,4.2,21.2,4.2c29.2,0,42.1-20.9,42.1-46.6V0H175.6z"/>
-    <g id="leafsGroup">
-      <defs>
-        <circle id="clipCircle" cx="64.3" cy="67.5" r="64.3"/>
-      </defs>
-      <clipPath id="clipCircle_1_">
-        <use xlink:href="#clipCircle"  style="overflow:visible;"/>
-      </clipPath>
-      <g id="leafs">
-        <circle id="leafBg" class="st3" cx="64.3" cy="67.5" r="106.3"/>
-        <g id="leafSpin">
-          <rect id="leaf6" x="-412" y="-130.4" transform="matrix(0.9659 -0.2588 0.2588 0.9659 -29.248 -48.3318)" class="st4" width="427.7" height="434.7"/>
-          <rect id="leaf5" x="-263.9" y="87.2" transform="matrix(0.2588 -0.9659 0.9659 0.2588 -331.2918 177.3486)" class="st4" width="427.7" height="434.7"/>
-          <rect id="leaf4" x="-1.4" y="67.7" transform="matrix(-0.7071 -0.7071 0.7071 -0.7071 161.0604 636.9291)" class="st4" width="427.7" height="434.7"/>
-          <rect id="leaf3" x="113" y="-169.3" transform="matrix(-0.9659 0.2588 -0.2588 -0.9659 655.0333 9.8304)" class="st4" width="427.7" height="434.7"/>
-          <polygon id="leaf2" class="st4" points="24.2,-432.4 -45.9,-170.9 1,4.1 333.4,93.2 444.1,-319.9 "/>
-          <polygon id="leaf1" class="st4" points="-47.9,-178.4 14.4,54.1 221.1,-152.7 -81.3,-455.1 -388.7,-147.7 -340.9,-99.9 "/>
-        </g>
-      </g>
-    </g>
-  </symbol>
-</svg>
 </template>
 
 <script>
+import Logo from "./components/Logo.vue";
 import { ref, computed } from "vue";
 import { useRouter, useRoute } from 'vue-router'
 
@@ -89,6 +51,9 @@ export default {
       isMobileMenu,
       doMenu,
     }
+  },
+  components: {
+    Logo
   }
 }
 </script>
@@ -111,78 +76,6 @@ export default {
   }
   .logo--m {
       cursor: pointer
-  }
-  .st{
-    opacity: 1;
-    fill: #fff;
-  }
-	.st0{
-    opacity:0.1;
-    fill:#F2F2F2;
-  }
-	#leafs{
-    clip-path:url(#clipCircle_1_);
-  }
-  #jpGroup{
-    clip-path:url(#clipJP_1_);
-  }
-  #leafSpin{
-    transform: scale(2);
-    transform-origin: center;
-    transform-box: fill-box;
-    animation: 
-      logoSpin
-      $logo-shutter-spin-duration
-      cubic-bezier(.74,.01,.48,1.01)
-      $logo-start-at
-      1 normal forwards running;
-  }
-  #clipJP{
-    transform-origin: center;
-    transform-box: fill-box;
-    animation: 
-      enlargeBg 3s 
-      cubic-bezier(0,.64,.33,.99) 
-      calc(#{$logo-start-at} + #{$logo-shutter-spin-duration} - .3s ) 
-      1 normal forwards running;
-  }
-	.st3{
-    opacity: .1;
-    fill: #fff;
-    transform-origin: center;
-    transform-box: fill-box;
-    animation: 
-      bgCircle
-      calc(#{$logo-start-at} + #{$logo-shutter-spin-duration}) 
-      cubic-bezier(.74,.01,.48,1.01)
-      0s
-      1 normal forwards running;
-  }
-  #J, #bgJ{
-    transform-origin: center;
-    transform-box: fill-box;
-    animation: 
-      alphabetSpin
-      2s 
-      cubic-bezier(.02,.71,.43,.97)
-      calc(#{$logo-start-at} + #{$logo-shutter-spin-duration} - .15s) 
-      1 normal forwards running;
-  }
-  #P, #bgP{
-    transform-origin: center;
-    transform-box: fill-box;
-    animation: 
-      alphabetSpin
-      2s 
-      cubic-bezier(.02,.71,.43,.97)
-      calc(#{$logo-start-at} + #{$logo-shutter-spin-duration} + .25s ) 
-      1 normal forwards running;
-  }
-	.st4{
-    fill:#FFFFFF;
-    stroke:#0F0F0F;
-    stroke-width:2;
-    stroke-miterlimit:10;
   }
   .mobileBg {
     background-color: $color-bg;
