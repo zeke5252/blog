57d17dfc4cbea820c91d2df15534f9d197d2156d
Zeke Lin <zeke5252@yahoo.com.tw>
2021-12-28 18:43:39 +0800

    Adjust loading component position

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container-fluid">
-    <div class="row mt-5">
-      <main class="col-12 col-md-11 offset-md-1 vh-100 d-flex">
+  <div class="container-fluid h-100">
+    <div class="row pt-5 h-100">
+      <main class="col-12 col-md-11 offset-md-1 d-flex h-100">
         <router-view />
       </main>
       <div
